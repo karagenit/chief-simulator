ff230cc71e40f8d337a4b50c740f6ee950a2feb0
Caleb Smith <karagenit@outlook.com>
2018-08-31 11:20:35 -0400

    Add Database Skeleton

---
+++ database.rb
@@ -0,0 +1,18 @@
+require 'sqlite3'
+
+class Database
+  def initialize
+  end
+
+  def get_last_post
+  end
+
+  def save_post
+  end
+
+  def get_post(post_id)
+  end
+
+  def get_post_contents
+  end
+end
