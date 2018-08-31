3bdd4577e3472c8aeb6cd8e6cb4a0fe7c2661999
Caleb Smith <karagenit@outlook.com>
2018-08-31 12:49:37 -0400

    Fix Bug in Download Script

--- download.rb
+++ download.rb
@@ -1,16 +1,17 @@
 #!/usr/bin/env ruby
 
-require 'database'
-require 'chief-delphi'
+require_relative './database.rb'
+require_relative './chief-delphi.rb'
 
 db = Database.new
 
 (1500000..1800000).each do |id|
+  puts "Post #{id}..."
   begin
     post = download_post(id)
     db.save_post(post)
   rescue => error
-    puts "Error Downloading/Saving Post #{id}: #{error.lines.first}"
+    puts "Error Downloading/Saving Post #{id}: #{error.to_s.lines.first}"
   end
 end
 
